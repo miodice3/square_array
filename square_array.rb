0494b85913d1c08d01ff35928e9936bfaa78defa
Michael Iodice <miodice3@gmail.com>
2020-09-30 11:01:27 -0600

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,13 @@
+#square_array, that squares each element in an array of numbers and returns a new array of these squared numbers
+
 def square_array(array)
   # your code here
+  new_array = []
+  array.each do |index|
+    new_array << (index**2)
+    #puts new_array
+  end
+  new_array
 end
+#array = [1,2,3]
+#square_array(array)
